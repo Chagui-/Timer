1384fb724df3d84ee65440be544ffcb4cb6ebb11
Chagui- <apeciro@gmail.com>
2014-04-19 21:39:57 -0400

    Fixed bug and refactored code.

--- Timer.cpp
+++ Timer.cpp
@@ -60,19 +60,7 @@ int8_t Timer::after(unsigned long period, void (*callback)())
 
 int8_t Timer::oscillate(uint8_t pin, unsigned long period, uint8_t startingValue, int repeatCount)
 {
-	int8_t i = findFreeEventIndex();
-	if (i == NO_TIMER_AVAILABLE) return NO_TIMER_AVAILABLE;
-
-	_events[i].eventType = EVENT_OSCILLATE;
-	_events[i].pin = pin;
-	_events[i].period = period;
-	_events[i].pinState = startingValue;
-	//digitalWrite(pin, startingValue);
-	_events[i].repeatCount = repeatCount * 2; // full cycles not transitions
-	_events[i].lastEventTime = millis();
-	_events[i].count = -1;
-	_events[i].delay = 0;
-	return i;
+	oscillate(pin, 0, period, startingValue, repeatCount)
 }
 
 int8_t Timer::oscillate(uint8_t pin, uint32_t delay, unsigned long period, uint8_t startingValue, int repeatCount)
@@ -84,7 +72,6 @@ int8_t Timer::oscillate(uint8_t pin, uint32_t delay, unsigned long period, uint8
 	_events[i].pin = pin;
 	_events[i].period = period;
 	_events[i].pinState = startingValue;
-	//digitalWrite(pin, startingValue);
 	_events[i].repeatCount = repeatCount * 2; // full cycles not transitions
 	_events[i].lastEventTime = millis();
 	_events[i].count = -1;
@@ -112,10 +99,10 @@ int8_t Timer::pulse(uint8_t pin, unsigned long period, uint8_t startingValue)
  */
 int8_t Timer::pulse(uint8_t pin,uint32_t delay, unsigned long period, uint8_t pulseValue)
 {
-	int8_t id(oscillate(pin, period, !pulseValue, 1));
+	int8_t id(oscillate(pin,delay, period, pulseValue, 1));
 	// now fix the repeat count
 	if (id >= 0 && id < MAX_NUMBER_OF_EVENTS) {
-		_events[id].delay = delay;
+		_events[id].repeatCount = 1;
 	}
 	return id;
 }
